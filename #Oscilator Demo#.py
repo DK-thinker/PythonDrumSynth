18c6096d081899489a0afa3d2584ba844998847c
daymonCKerns <165515582+daymonCKerns@users.noreply.github.com>
2024-03-30 16:21:55 -0400

    Add files via upload

---
+++ #Oscilator Demo#.py
@@ -0,0 +1,12 @@
+#Oscilator Demo#
+#Used Ideas/code from https://plainenglish.io/blog/making-a-synth-with-python-oscillators-2cb8e68e9c3b,
+#https://github.com/18alantom/synth/blob/main/Code%20Oscillators.ipynb 
+#Specifically the implementation of oscilator objects 
+
+import pyaudio
+import math
+
+class Oscilator:
+    def __init__(self, freq, amp=1, sampleRate=44100,
+                 waveRange=(-1,1)):
+        pass
