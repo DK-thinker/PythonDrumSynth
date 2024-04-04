1801edff18b3d193d91539b8495268016b750c69
daymonCKerns <165515582+daymonCKerns@users.noreply.github.com>
2024-04-04 00:28:22 -0400

    Started building my own Oscillator object and got it working with pyaudio, ready to begin scaling and implementing ASDR

--- OOP-Oscilator Demo#.py
+++ OOP-Oscilator Demo#.py
@@ -1,46 +1,79 @@
 #Oscilator Demo#
 #Used Ideas/code from https://plainenglish.io/blog/making-a-synth-with-python-oscillators-2cb8e68e9c3b, --- published by 'Alan'
 #https://github.com/18alantom/synth/blob/main/Code%20Oscillators.ipynb ---- published by 'Alan'
-#Specifically the archetecture of oscilator objects 
-#In my project though I plan on creating an original 'sequencer' object 
+# Specifically I read through and learned his approach to an OOP-based oscilator, 
+# but I tried to build my Oscilator without directly copying him, 
+# but there was inspiration. And any backend type of audio code I took from him
+# will be directly stated 
 
+import numpy as np
+from cmu_graphics import *
 import pyaudio
 import math
 import itertools
 
+p = pyaudio.PyAudio()
+
+
+stream = p.open(output=True,
+                channels=1,
+                rate=44100,
+                format=pyaudio.paInt16,
+                frames_per_buffer=256
+                )
+# print(type(stream))
+
 class Oscillator:
-    def __init__(self, freq, amp=1, sampleRate=44100,
-                 waveRange=(-1,1)):
-        pass
 
-def makeSinWave(freq, amp=1, samplerate=44100):            #From Alan
-    increment = (2*math.pi * freq) / samplerate
-    return ((math.sin(v) * amp) for v in itertools.count(start=0, step=increment))
+    def __init__(self, freq, amp=1, sampleRate=44100):
+        self.freq = freq
+        self.amp = amp
+        self.sampleRate = sampleRate
+        self.stepRate = (2*math.pi * self.freq) / self.sampleRate
+    
 
-osc = makeSinWave(freq=440)
-samples = [next(osc) for i in range(512)]
-print(samples)
 
-class SineOscillator(Oscillator):
-    def _post_freq_set(self):
-        self.step = (2 * math.pi * self.f) / self.sampleRate
+class sinOscillator(Oscillator):
+
+    def __init__(self, freq, amp=1, sampleRate=44100):
+        super().__init__(freq)
+        self.position = 0
+
+    def generateWave(self):             #From Alan
+        increment = (2 * math.pi * self.freq)/ self.sampleRate
+        return (math.sin(v) * self.amp for v in itertools.count(start=0, step=increment))
+    
+    def getSamples(self):               #From Alan
+        wave = self.generateWave()
+        return [int(next(wave) * 32767) for i in range(256*44100)]
+
+
+
+def onAppStart(app):
+    app.playing = False
+
+
+def onKeyPress(app, key):
+    if key == 'p':
+        print(app.playing)
+        if not (app.playing):
+            app.playing = True
+            # print('playing')
+            # stream.start_stream()
+            note = sinOscillator(freq=80, amp=1)
+            data = np.int16(note.getSamples()).tobytes()
+            print(data)
+            stream.write(data)
+        elif app.playing:
+            app.playing = False
+            # stream.close()
         
-    def _post_phase_set(self):
-        self._p = (self._p / 360) * 2 * math.pi
-        
-    def _initialize_osc(self):
-        self._i = 0
-        
-    def __next__(self):
-        val = math.sin(self._i + self._p)
-        self._i = self._i + self._step
-        if self.waveRange is not (-1, 1):
-            val = self.squish_val(val, *self._wave_range)
-        return val * self._a
+        # print(note.getSamples())
 
 
-stream = p.open(format = p.get_format_from_width(1), 
-                channels = 1, 
-                rate = BITRATE, 
-                output = True)
+def main():
+    # openStream()
+    # print('HERE', p.get_default_output_device_info())
+    runApp()
 
+main()
